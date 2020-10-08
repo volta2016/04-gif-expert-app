76c1d74cfdacb6011b8e99fd3a0db7c3e54c2368
Volta <giovoltadesign@gmail.com>
2020-10-07 23:51:05 -0300

    adding animation

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -9,10 +9,10 @@ const GifGrid = ({category}) => {
 
     return (
         <>
-            <h3>{ category }</h3>
+            <h3 className="card-grid animate__animated animate__fadeIn">{ category }</h3>
 
-            { loading && <p>Loading</p>}
-            <div className="card-grid">
+            { loading && <p className="animate__animated animate__flash">Loading</p>}
+            <div className="card-grid" >
                 {
                     images.map((img) => (
                         <GifGridItem 
