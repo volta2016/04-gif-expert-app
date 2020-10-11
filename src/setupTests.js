ff791b491551a12f92e228bd239a6a0a5f76276e
Volta <giovoltadesign@gmail.com>
2020-10-11 12:13:22 -0300

    starting with unit testing - first snapshoot

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {createSerializer} from 'enzyme-to-json';
+// import '@testing-library/jest-dom/extend-expect';
+ 
+
+Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+
+
---
+++ src/test/components/GifGridItem.test.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import '@testing-library/jest-dom';
+import {shallow} from 'enzyme';
+import GifGridItem  from '../../components/GifGridItem';
+
+
+describe('Pruebas en <GifGridItem />', () => {
+  
+  test('debe de mostrar el componente correctamente', () => {
+    const wrapper  = shallow(<GifGridItem />)
+    expect(wrapper).toMatchSnapshot(); 
+  })
+  
+});
+
