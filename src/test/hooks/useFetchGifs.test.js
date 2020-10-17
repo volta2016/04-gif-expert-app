ceb548e0ba10dce1e16d7270d171edfa8bdbf222
Volta <giovoltadesign@gmail.com>
2020-10-17 11:03:11 -0300

    waitForNextUpdate()

--- src/test/hooks/useFetchGifs.test.js
+++ src/test/hooks/useFetchGifs.test.js
@@ -4,15 +4,27 @@ import {renderHook} from '@testing-library/react-hooks'
 
 describe('pruebas en hook useFetchGifs', () => {
   
-  test('debe de retornar el estado inicial', () => {
+  test('debe de retornar el estado inicial', async() => {
     
-    const {result }= renderHook( () => useFetchGifs('One Punch'))
+    const { result, waitForNextUpdate  } = renderHook( () => useFetchGifs('One Punch'))
     const {data, loading} = result.current
-    
+    await waitForNextUpdate();
     
     expect(data).toEqual([]);
     expect(loading).toBe(true);
   })
+
+  test('debe de retornar un arreglo de imgs y en loading en false', async() => {
+    const {result, waitForNextUpdate   } = renderHook( () => useFetchGifs('One Punch'))
+    await waitForNextUpdate();
+    const {data, loading} = result.current
+    
+    
+    expect(data.length).toBe(10);
+    expect(loading).toBe(false);
+    
+  })
+  
   
   
 });
