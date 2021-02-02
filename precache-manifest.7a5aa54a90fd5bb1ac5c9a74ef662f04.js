699d055dd2f178e516b56faa7e8c0d119ffce884
Volta <giovoltadesign@gmail.com>
2021-02-02 02:36:40 -0300

    Updates

---
+++ precache-manifest.7a5aa54a90fd5bb1ac5c9a74ef662f04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2ad0e6dfa28a12dd100312803d400a",
+    "url": "/04-gif-expert-app/index.html"
+  },
+  {
+    "revision": "4301d75c387bbdd7ec26",
+    "url": "/04-gif-expert-app/static/css/main.d930af71.chunk.css"
+  },
+  {
+    "revision": "101f7b16983c51035cb7",
+    "url": "/04-gif-expert-app/static/js/2.ceb28f97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/04-gif-expert-app/static/js/2.ceb28f97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4301d75c387bbdd7ec26",
+    "url": "/04-gif-expert-app/static/js/main.16dc9352.chunk.js"
+  },
+  {
+    "revision": "2a84d61969e79d013f49",
+    "url": "/04-gif-expert-app/static/js/runtime-main.b0935dcc.js"
+  },
+  {
+    "revision": "640560cd6b1cf39d88047465f38337db",
+    "url": "/04-gif-expert-app/static/media/logo-gif-expert-app.640560cd.svg"
+  },
+  {
+    "revision": "9f64d7836cc0c1c23267015ce1ad74f6",
+    "url": "/04-gif-expert-app/static/media/search.9f64d783.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/04-gif-expert-app/precache-manifest.7a5aa54a90fd5bb1ac5c9a74ef662f04.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/04-gif-expert-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
